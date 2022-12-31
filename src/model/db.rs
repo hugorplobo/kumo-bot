3e51929a60959ce506a5bfa26998caacb0165ede
Antônio Hugo <hugorplobo@gmail.com>
2022-12-31 17:13:55 -0300

    feat: remove trait and add postgres implementation

--- src/model/db.rs
+++ src/model/db.rs
@@ -1,10 +1,74 @@
 use std::collections::HashMap;
 
+use log::error;
+
+use crate::DbPool;
+
 use super::file::File;
 
-pub trait Database {
-    fn add(&mut self, user_id: &str, file: &File) -> Result<(), ()>;
-    fn remove(&mut self, user_id: &str, file_id: &str) -> Result<(), ()>;
-    fn get(&self, user_id: &str, file_id: &str) -> Result<File, ()>;
-    fn get_all(&self, user_id: &str) -> Result<HashMap<String, File>, ()>;
+pub struct Database {
+    pool: DbPool
+}
+
+impl Database {
+    pub fn new(pool: DbPool) -> Self {
+        Database { pool }
+    }
+
+    pub async fn add(&self, user_id: &str, file: &File) -> Result<(), ()> {
+        let conn = self.pool.get().await.unwrap();
+        
+        if let Err(err) = conn.execute(
+            "insert into file values ($1::TEXT, $2::TEXT)",
+            &[&file.id, &file.name]
+        ).await {
+            error!("Failed to insert file: {err}");
+            return Err(());
+        }
+
+        if let Err(err) = conn.execute(
+            "insert into file_user values ($1::TEXT, $2::TEXT)",
+            &[&user_id, &file.id]
+        ).await {
+            error!("Failed to bind file to user: {err}");
+            return Err(());
+        }
+
+        Ok(())
+    }
+
+    pub async fn remove(&self, user_id: &str, file_id: &str) -> Result<(), ()> {
+        let conn = self.pool.get().await.unwrap();
+
+        if let Err(err) = conn.execute(
+            "delete from file_user where id_user = $1::TEXT and id_file = $2::TEXT",
+            &[&user_id, &file_id]
+        ).await {
+            error!("Failed to remove file from user: {err}");
+            return Err(());
+        }
+
+        Ok(())
+    }
+
+    pub async fn get(&self, file_id: &str) -> Result<File, ()> {
+        let conn = self.pool.get().await.unwrap();
+
+        match conn.query(
+            "select from file where id = $1::TEXT",
+            &[&file_id]
+        ).await {
+            Ok(rows) => {
+                return Ok(File::new(rows[0].get("id"), rows[0].get("name")));
+            },
+            Err(err) => {
+                error!("Failed to get file: {err}");
+                return Err(());
+            }
+        }
+    }
+
+    pub fn get_all(&self, _user_id: &str) -> Result<HashMap<String, File>, ()> {
+        todo!()
+    }
 }
