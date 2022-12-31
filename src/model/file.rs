b635a5e9736e2b3a1bb1194907547effdc7f9062
Antônio Hugo <hugorplobo@gmail.com>
2022-12-31 17:14:15 -0300

    feat: add method new

--- src/model/file.rs
+++ src/model/file.rs
@@ -1,5 +1,11 @@
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct File {
     pub id: String,
     pub name: String,
+}
+
+impl File {
+    pub fn new(id: &str, name: &str) -> Self {
+        File { id: id.to_string(), name: name.to_string() }
+    }
 }
