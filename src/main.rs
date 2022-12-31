3b42d07b97e488573deb7348d8e3a4c3d5f079fc
Antônio Hugo <hugorplobo@gmail.com>
2022-12-31 17:58:32 -0300

    chore: get db params from env vars

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use frankenstein::{AsyncTelegramApi, AsyncApi, GetUpdatesParams};
 
 pub mod model;
 pub mod handlers;
+pub mod utils;
 
 use handlers::command_handler::parse_update;
 
@@ -19,17 +20,21 @@ async fn main() {
     env_logger::init();
     
     info!("Starting api");
-    let token = env::var("TOKEN").expect("The telegram token is necessary");
+    let token = env::var("TELEGRAM_TOKEN").expect("The telegram token is necessary");
     let bot = AsyncApi::new(&token);
 
     info!("Starting database connection pool");
     let mut db_config = Config::new();
+    let pg_user = env::var("PG_USER").expect("The postgres username is necessary");
+    let pg_password = env::var("PG_PASSWORD").expect("The postgres password is necessary");
+    let pg_db_name = env::var("PG_DB_NAME").expect("The postgres db name is necessary");
+    let pg_hostname = env::var("PG_HOSTNAME").expect("The postgres hostname is necessary");
 
     db_config
-        .user("postgres")
-        .password("postgres")
-        .dbname("kumo")
-        .host("localhost");
+        .user(&pg_user)
+        .password(&pg_password)
+        .dbname(&pg_db_name)
+        .host(&pg_hostname);
 
     let manager = bb8_postgres::PostgresConnectionManager::new(
         db_config,
@@ -43,6 +48,8 @@ async fn main() {
 
     let update_params_builder = GetUpdatesParams::builder();
     let mut update_params = update_params_builder.clone().build();
+
+    info!("Running");
 
     loop {
         if let Ok(response) = bot.get_updates(&update_params).await {
