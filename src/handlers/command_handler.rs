90e1aa59b9f9a350423df2a10563d9df86b05156
Antônio Hugo <hugorplobo@gmail.com>
2022-12-31 18:00:57 -0300

    feat: add command handler

---
+++ src/handlers/command_handler.rs
@@ -0,0 +1,30 @@
+use frankenstein::{AsyncApi, Update, UpdateContent};
+use log::info;
+
+use crate::{handlers::{help::handle_help, add::handle_add, list::handle_list}, DbPool, model::db::Database};
+
+pub async fn parse_update(bot: AsyncApi, update: Update, pool: DbPool) {
+    let db = Database::new(pool);
+
+    match update.content {
+        UpdateContent::Message(msg) => {
+            if let Some(ref text) = msg.text {
+                if text.starts_with("/help") {
+                    info!("Help message received");
+                    handle_help(&bot, &msg).await;
+                } else if text.starts_with("/list") {
+                    info!("List message received");
+                    handle_list(&bot, &msg, &db).await;
+                } else if text.starts_with("/id") {
+                    info!("Id message received");
+                }
+            } else if let Some(_) = msg.document {
+                info!("Document received");
+                handle_add(&bot, &msg, &db).await;
+            }
+        },
+        _ => {
+            info!("Unknown response received");
+        }
+    }
+}
