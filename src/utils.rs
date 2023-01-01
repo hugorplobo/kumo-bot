f3d8d04cbcb98ac3176b1ede05b16830dbfbb54e
Antônio Hugo <hugorplobo@gmail.com>
2023-01-01 00:01:24 -0300

    feat: add create keyboard function

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,29 @@
+use frankenstein::{InlineKeyboardButton, InlineKeyboardMarkup};
+
 pub fn escape_filename(name: &str) -> String {
     let res = String::from(name);
     res.replace(".", "\\.")
+        .replace("_", "\\_")
+}
+
+pub fn create_inline_keyboard(btns: Vec<(&str, &str)>) -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = Vec::new();
+    let mut row: Vec<InlineKeyboardButton> = Vec::new();
+
+    for btn in btns {
+        let button = InlineKeyboardButton::builder()
+            .text(btn.0)
+            .callback_data(btn.1)
+            .build();
+        
+        row.push(button);
+    }
+
+    keyboard.push(row);
+
+    let inline_keyboard = InlineKeyboardMarkup::builder()
+        .inline_keyboard(keyboard)
+        .build();
+    
+    inline_keyboard
 }
