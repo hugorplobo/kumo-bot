b77d0e72afded468a1b3e026f08a3b9a79636713
Antônio Hugo <hugorplobo@gmail.com>
2023-01-01 03:40:18 -0300

    feat: update file struct

--- src/model/file.rs
+++ src/model/file.rs
@@ -1,11 +1,12 @@
 #[derive(Clone, Debug)]
 pub struct File {
-    pub id: String,
+    pub id: i32,
+    pub telegram_id: String,
     pub name: String,
 }
 
 impl File {
-    pub fn new(id: &str, name: &str) -> Self {
-        File { id: id.to_string(), name: name.to_string() }
+    pub fn new(telegram_id: &str, name: &str) -> Self {
+        File { id: 0, telegram_id: telegram_id.to_string(), name: name.to_string() }
     }
 }
