d185fcfd8b08c56352eb8183549f3e98de50aa6f
Antônio Hugo <hugorplobo@gmail.com>
2023-01-02 00:34:10 -0300

    feat: add inline query handler

--- src/handlers/query_handler.rs
+++ src/handlers/query_handler.rs
@@ -1,11 +1,11 @@
-use frankenstein::{CallbackQuery, AsyncApi};
+use frankenstein::{CallbackQuery, AsyncApi, InlineQuery, AnswerInlineQueryParams, InlineQueryResultCachedDocument, InlineQueryResult, AsyncTelegramApi, InlineQueryResultArticle, InputTextMessageContent};
 use log::{error, info};
 
 use crate::{model::db::Database, handlers::{back::handle_back, delete::handle_delete}};
 
 use super::next::handle_next;
 
-pub async fn parse_query(bot: &AsyncApi, query: &CallbackQuery, db: &Database) {
+pub async fn parse_callback_query(bot: &AsyncApi, query: &CallbackQuery, db: &Database) {
     if let Some(_) = query.message {
         if let Some(ref data) = query.data {
             let values: Vec<_> = data.split(",").collect();
@@ -27,6 +27,54 @@ pub async fn parse_query(bot: &AsyncApi, query: &CallbackQuery, db: &Database) {
                     error!("Unknown callback query type");
                 }
             }
+        }
+    }
+}
+
+pub async fn parse_inline_query(bot: &AsyncApi, query: &InlineQuery, db: &Database) {
+    let user_id = query.from.id.to_string();
+
+    if let Ok(files) = db.search(&user_id, &query.query).await {
+        let mut results: Vec<InlineQueryResult> = files.iter().map(|file| {
+            let result = InlineQueryResult::CachedDocument(
+                InlineQueryResultCachedDocument::builder()
+                    .id(file.id.to_string())
+                    .title(file.name.clone())
+                    .document_file_id(file.telegram_id.clone())
+                    .build()
+            );
+            
+            result
+
+        }).collect();
+
+        if results.len() < 1 {
+            results.push(
+                InlineQueryResult::Article(
+                    InlineQueryResultArticle::builder()
+                        .id("0")
+                        .title(if query.query == "" { "No files saved yet" } else { "No file found for this search" })
+                        .input_message_content(
+                            frankenstein::InputMessageContent::Text(
+                                InputTextMessageContent::builder()
+                                    .message_text(if query.query == "" { "Send me a file to be saved :D" } else { "Make sure you didn't make a typo, and remember that you can see all your files with /list" })
+                                    .build()
+                            )
+                        )
+                        .build()
+                )
+            );
+        }
+
+        let params = AnswerInlineQueryParams::builder()
+            .inline_query_id(query.id.clone())
+            .is_personal(true)
+            .cache_time(0u32)
+            .results(results)
+            .build();
+        
+        if let Err(err) = bot.answer_inline_query(&params).await {
+            error!("Failed to show files to user: {err}");
         }
     }
 }
